3b5b40a82b2c5663a0cdb56fa2a3e90515ccadb8
RafaelHub <raffiscoffe@gmail.com>
2025-03-20 23:26:18 +0000

    refi_1.1

--- refi/refifive.c
+++ refi/refifive.c
@@ -18,7 +18,7 @@ void linhas_pequenas(){
 
 int main(){
     srand(time(0));
-    int opcao,palpite_numb,computer;
+    int opcao,palpite_numb,computer,jogador_1,jogador_2,resultado;
 
     linhas_pequenas();
     printf("### BEM VINDO! ###\n");
@@ -56,21 +56,45 @@ int main(){
             if (opcao == 1){
                 
                 computer = rand() % 10 + 1;
+                linhas_pequenas();
                 printf("Você quer jogar com o computador, muito bom!\n");
                 printf("Vamos lá então, digite um número de 1 até 10\n");
                 scanf("%d", &palpite_numb);
 
                 if(palpite_numb == computer){
+                    linhas_pequenas();
                     printf("O seu número foi (%d)\n", palpite_numb);
-                    printf("O número do computafor foi (%d)\n", computer);
-                    printf("Você acertou o número!\n");
+                    printf("O número do computador foi (%d)\n", computer);
+                    printf("Parabéns, você acertou o número!\n");
                 }
                 else{
                     printf("O seu número foi (%d)\n", palpite_numb);
-                    printf("O número do computafor foi (%d)\n", computer);
-                    printf("Você errou o número!\n");
+                    printf("O número do computador foi (%d)\n", computer);
+                    printf("Que pena, mas você perdeu!\n");
                 }
             }
+            else if (opcao == 2){
+                printf("Para este modo se recomenda que leia as regras no menu antes de iniciar\n");
+                printf("Jogador_01:");
+                scanf("%d", &jogador_1);
+                printf("Jogador_02: ");
+                scanf("%d", &jogador_2);
+
+                resultado = jogador_1 == jogador_2 ? 1 : 0;
+                
+                if (resultado == 1){
+                    printf("O jogador_01 digitou (%d)\n", jogador_1);
+                    printf("O jogador_02 digitou (%d)\n", jogador_2);
+                    printf("Houve um Ruffis!!! Parabéns\n");
+
+                }
+                else{
+                    printf("O jogador_01 digitou (%d)\n", jogador_1);
+                    printf("O jogador_02 digitou (%d)\n", jogador_2);
+                    printf("Que pena, não foi desta vez!\n");
+                }
+            }
+
             break;
         case 2:
             break;
