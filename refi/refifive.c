1d1238868214d696db6866d0338a06416993bba8
RafaelHub <raffiscoffe@gmail.com>
2025-03-20 23:56:25 +0000

    refi_1.2

--- refi/refifive.c
+++ refi/refifive.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 
 void linhas_grandes(){
@@ -97,6 +98,34 @@ int main(){
 
             break;
         case 2:
+        linhas_grandes();
+            printf("Bem-vindo ao REFI FIVE!\n"
+                   "Esta parte do jogo é dedicada a explicar as regras para o jogo escolhido.\n"
+                   "O jogo de Adivinhação é muito divertido e traz algumas mecânicas interessantes. Vamos às regras:\n");
+            linhas_pequenas();
+            printf("1. Modo Jogador x Computador\n"
+                   "Se você optar por jogar contra o computador, é bem simples:\n"
+                   "basta escolher um número entre 1 e 10. O computador, por sua vez, irá gerar um número aleatório\n"
+                   "usando um algoritmo de geração pseudoaleatória (LCG – Linear Congruential Generator).\n"
+                   "Após isso, vamos comparar o número digitado por você com o número sorteado pelo computador e ver quem ganhou!\n");
+            linhas_pequenas();
+            printf("2. Modo Jogador x Jogador\n"
+                   "Se você escolher o modo de dois jogadores, o importante é que tenha alguém para jogar com você, pois isso\n"
+                   "tornará a experiência mais divertida e competitiva! Cada jogador deve escolher um número entre 1 e 10.\n"
+                   "Se os dois jogadores acertarem o número, ocorre um evento especial chamado RUFFIS, um momento raro e emocionante onde ambos escolhem o mesmo número.\n"
+                   "Isso não acontece com o computador, já que os números do computador são gerados aleatoriamente, mas entre humanos, as escolhas podem coincidir.\n");
+            linhas_pequenas();
+            printf("O que significa (RUFFIS)?\n"
+                   "RUFFIS é uma sigla que descreve o momento em que os dois jogadores acertam o mesmo número. Cada letra tem um significado:\n");
+            printf("R: Refletir\n");
+            printf("U: Unir\n");
+            printf("F: Fazer\n");
+            printf("F: Fiz\n");
+            printf("I: Irreal\n");
+            printf("R: Real\n");
+            printf("Essas palavras representam o conceito de um momento raro e único no jogo.\n"
+                   "Se você não entendeu totalmente, não se preocupe – só jogando você entenderá completamente o que significa RUFFIS!\n");
+            linhas_grandes();
             break;
         case 3:
             break;
